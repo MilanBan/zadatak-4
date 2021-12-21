848c512e189ed803e84d7a770bd2bb61b8b8ca11
Milan Bancevic <m.bancevic@gmail.com>
2021-12-21 19:14:41 +0100

    implement basic Register model & validation

--- views/register.php
+++ views/register.php
@@ -3,22 +3,21 @@
 <form action="" method="post">
   <div class="row">
     <div class="col mb-3">
-      <input type="firstName" name="firstName" class="form-control" placeholder="First Name">
+      <input type="username" name="username" class="form-control" placeholder="Username">
     </div>
     <div class="col mb-3">
-      <input type="lastName" name="lastName" class="form-control" placeholder="Last Name">
+      <input type="email" name="email" class="form-control" placeholder="email">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
   </div>
-  <div class="mb-3">
-    <input type="email" name="email" class="form-control" placeholder="email">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  <div class="row">
+    <div class="col mb-3">
+      <input type="password" name="password" class="form-control" placeholder="Password">
+    </div>
+    <div class="col mb-3">
+      <input type="password" name="password_confirm" class="form-control" placeholder="Confirm Password">
+    </div>
   </div>
-  <div class="mb-3">
-    <input type="password" name="password" class="form-control" placeholder="Password">
-  </div>
-  <div class="mb-3">
-    <input type="password" name="password_confirm" class="form-control" placeholder="Confirm Password">
-  </div>
-
+  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
